b32281131f4f594dae09a765afcaee96dafe3580
alexey <alexnagorny.an@gmail.com>
2025-02-09 01:50:47 +0000

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,13 @@ import { HashRouter as Router } from "react-router-dom";
 import "normalize.css";
 import { ToastContainer } from "react-toastify";
 import { createRoot } from "react-dom/client";
-import { PrivyProvider } from "@privy-io/react-auth";
+import { PrivyClientConfig, PrivyProvider } from "@privy-io/react-auth";
 import { PRIVY_APP_ID, PRIVY_CONFIG }  from "./utils/constants";  
 
 createRoot(document.getElementById("root")!).render(
   <PrivyProvider
     appId={PRIVY_APP_ID}
-    config={PRIVY_CONFIG}
+    config={PRIVY_CONFIG as PrivyClientConfig}
   >
     <Router>
       <App />
